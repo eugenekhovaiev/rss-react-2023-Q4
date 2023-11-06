a1ab24d00bd5150d8db51206cf426cb44495ef2b
Yevheniy Khovaiev <ekhovaev@gmail.com>
2023-11-06 02:00:59 +0200

    feat: add basic router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,10 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Main from '../pages/main/Main';
 
+const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<Main />} />));
+
 function App(): JSX.Element {
-  return <Main />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
